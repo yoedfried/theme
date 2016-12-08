7e04afd0c28f35bb1b9811cb9ef4ae24e1b2f524
Himanshu <himanshu@sahusoft.com>
2016-12-08 12:39:47 +0530

    fixed root ref

--- src/connectStyle.js
+++ src/connectStyle.js
@@ -144,7 +144,7 @@ export default (componentStyleName, componentStyle = {}, mapPropsToStyleNames, o
       }
 
       setWrappedInstance(component) {
-        if(component._root) {
+        if(component && component._root) {
           this._root = component._root;
         } else {
           this._root = component;
