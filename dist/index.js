8789996453ce0da8f93829f08ecf01f903c3f44d
Sankhadeep <sankhadeep@sahusoft.com>
2017-03-21 18:44:13 +0530

    theme removed

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,8 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.Examples=exports.createSharedStyle=exports.createVariations=exports.ThemeShape=exports.Theme=exports.StyleProvider=exports.INCLUDE=exports.connectStyle=undefined;var _connectStyle=require('./src/connectStyle');var _connectStyle2=_interopRequireDefault(_connectStyle);
+Object.defineProperty(exports,"__esModule",{value:true});exports.createSharedStyle=exports.createVariations=exports.ThemeShape=exports.Theme=exports.StyleProvider=exports.INCLUDE=exports.connectStyle=undefined;var _connectStyle=require('./src/connectStyle');var _connectStyle2=_interopRequireDefault(_connectStyle);
 var _resolveIncludes=require('./src/resolveIncludes');
 var _StyleProvider=require('./src/StyleProvider');var _StyleProvider2=_interopRequireDefault(_StyleProvider);
 var _Theme=require('./src/Theme');var _Theme2=_interopRequireDefault(_Theme);
-var _addons=require('./src/addons');
-var _Shapes=require('./examples/Shapes');var _Shapes2=_interopRequireDefault(_Shapes);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
-
-var Examples={
-Shapes:_Shapes2.default};exports.
-
+var _addons=require('./src/addons');function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.
 
 
 connectStyle=_connectStyle2.default;exports.
@@ -16,6 +11,5 @@ StyleProvider=_StyleProvider2.default;exports.
 Theme=_Theme2.default;exports.
 ThemeShape=_Theme.ThemeShape;exports.
 createVariations=_addons.createVariations;exports.
-createSharedStyle=_addons.createSharedStyle;exports.
-Examples=Examples;
+createSharedStyle=_addons.createSharedStyle;
 //# sourceMappingURL=index.js.map
